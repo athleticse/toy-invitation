2237f3239c3e3736b29923379a392b4b09bd9508
nollha <athleticse@naver.com>
2022-04-19 19:38:40 +0900

    최초 프로젝트 Commit

---
+++ src/main/kotlin/toy/invitation/toyinvitation/ToyInvitationApplication.kt
@@ -0,0 +1,11 @@
+package toy.invitation.toyinvitation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ToyInvitationApplication
+
+fun main(args: Array<String>) {
+	runApplication<ToyInvitationApplication>(*args)
+}
